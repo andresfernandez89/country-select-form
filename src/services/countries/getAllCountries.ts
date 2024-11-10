1499bb18527a454724f3b7fdf5540838d690e6e8
andresfernandez89 <andresfernandez.mdp@gmail.com>
2024-11-10 09:40:21 -0300

    feat: Modify countries endpoint logic and implement sorting

--- src/services/countries/getAllCountries.ts
+++ src/services/countries/getAllCountries.ts
@@ -4,7 +4,14 @@ import { getApiUrl } from "../config";
 export const getAllCountries = async () => {
   const response = await getApiUrl({
     path: "/countries/info?returns=flag",
-    method: "GET",
+    options: {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      },
+    },
   });
-  return response.data.map((country: ICountry) => country.name);
+  return response.data.data
+    .map((country: ICountry) => country.name)
+    .sort((a: string, b: string) => a.localeCompare(b));
 };
