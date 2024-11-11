7925963fb0c80e764ce9aeadb30db4c8dce9a451
andresfernandez89 <andresfernandez.mdp@gmail.com>
2024-11-10 23:40:21 -0300

    feat: Add initial environment variable validation

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/index.css";
 
+if (!import.meta.env.VITE_API_BASE_URL) {
+  throw new Error(
+    "No API_BASE_URL environment variable found. Please refer to README.md for more info.",
+  );
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
